3596cc3bd518658e416bb9c47346a6a8ab247e89
Misbahur <misbahur.rifqi61@gmail.com>
2024-09-05 16:43:11 +0700

    Create Filament-Resource And Litle Modified

---
+++ app/Filament/Resources/EmployeResource.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\EmployeResource\Pages;
+use App\Filament\Resources\EmployeResource\RelationManagers;
+use App\Models\Employe;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class EmployeResource extends Resource
+{
+    protected static ?string $model = Employe::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationLabel = 'Employe';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Section::make('Nama User')
+                    ->description('Put the user name details in.')
+                    ->schema([
+                        Forms\Components\TextInput::make('first_name')
+                            ->required()
+                            ->maxLength(255),
+                        Forms\Components\TextInput::make('last_name')
+                            ->required()
+                            ->maxLength(255),
+                        Forms\Components\TextInput::make('middle_name')
+                            ->required()
+                            ->maxLength(255),
+                    ])->columns(3),
+                // Forms\Components\TextInput::make('country_id')
+                //     ->required()
+                //     ->numeric(),
+                // Forms\Components\TextInput::make('state_id')
+                //     ->required()
+                //     ->numeric(),
+                // Forms\Components\TextInput::make('city_id')
+                //     ->required()
+                //     ->numeric(),
+                // Forms\Components\TextInput::make('department_id')
+                //     ->required()
+                //     ->numeric(),
+                Forms\Components\Section::make('Alamat User')
+                    ->description('Masukan Alamat Anda disini')
+                    ->schema([
+                        Forms\Components\TextInput::make('address')
+                            ->required()
+                            ->maxLength(255),
+                        Forms\Components\TextInput::make('zip_code')
+                            ->required()
+                            ->maxLength(255),
+                    ])->columns(2),
+                Forms\Components\Section::make('Lain - lain')
+                    ->description('Data Kelahiran dan Masuk nya usar')
+                    ->schema([
+                        Forms\Components\DatePicker::make('date_of_brith')
+                            ->required(),
+                        Forms\Components\DatePicker::make('date_of_hired')
+                            ->required(),
+                    ])->columns(2),
+            ])->columns(3);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('country_id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('state_id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('city_id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('department_id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('first_name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('last_name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('middle_name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('address')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('zip_code')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('date_of_brith')
+                    ->date()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('date_of_hired')
+                    ->date()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListEmployes::route('/'),
+            'create' => Pages\CreateEmploye::route('/create'),
+            'view' => Pages\ViewEmploye::route('/{record}'),
+            'edit' => Pages\EditEmploye::route('/{record}/edit'),
+        ];
+    }
+}
